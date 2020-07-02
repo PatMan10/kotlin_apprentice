32de3aa5590d9842f73dd41581e710db3c68b351
PatMan10 <PatMan10@protonmail.com>
2020-07-02 17:16:36 +0200

    added kotlin dirs, compiling and running

---
+++ src/main/kotlin/com/g_corp/kotlin_apprentice/App.kt
@@ -0,0 +1,7 @@
+package com.g_corp.kotlin_apprentice
+
+fun main(args: Array<String>) {
+    println("%.0f".format(28.0 % 10.0))
+    println("1 shl 3 = ${1 shl 3}")
+    println("32 shr 2 = ${1 shl 3}")
+}
---
+++ src/test/kotlin/com/g_corp/kotlin_apprentice/AppTest.kt
@@ -0,0 +1,15 @@
+package com.g_corp.kotlin_apprentice
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    fun shouldAnswerWithTrue() {
+        println("TEST MAN !!!!!!!!!!!!!!!!!!!!")
+        Assert.assertTrue(true)
+    }
+}
