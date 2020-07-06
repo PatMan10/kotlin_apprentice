415cd0818b8173c0058085bd338584c7187c7b68
PatMan10 <PatMan10@protonmail.com>
2020-07-06 07:44:00 +0200

    gona modify pom and see if i can run kotlin-compiler plugin alone

--- src/main/kotlin/com/g_corp/kotlin_apprentice/chapter_4/challenges/Question_04.kt
+++ src/main/kotlin/com/g_corp/kotlin_apprentice/chapter_4/challenges/Question_04.kt
@@ -1,5 +1,10 @@
 package com.g_corp.kotlin_apprentice.chapter_4.challenges
 
 fun main(args: Array<String>) {
-    println("Challenge Question")
+    val a = 1
+    val b = 3
+    val c = 4
+    val x1 = (-b + Math.sqrt(Math.pow(b.toDouble(), 2.0) - (4 * a * c)) / (2 * a))
+    val x2 = (-b - Math.sqrt(Math.pow(b.toDouble(), 2.0) - 4 * a * c)) / 2 * a
+    println("x1 = $x1 x2 = $x2")
 }
