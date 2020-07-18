1a9e87340980e43cdd32313c640e1276ceaa0952
PatMan10 <PatMan10@protonmail.com>
2020-07-18 15:02:01 +0200

    chapter 14 done

--- src/test/kotlin/com/g_corp/kotlin_apprentice/chapter_14/challenges/Question_05Test.kt
+++ src/test/kotlin/com/g_corp/kotlin_apprentice/chapter_14/challenges/Question_05Test.kt
@@ -5,7 +5,25 @@ import org.junit.Test
 
 public class Question_05Test {
     @Test
-    fun testSomething() {
-        Assert.assertEquals(1, 1)
+    fun testExtensionMethodPrimeFactors() {
+        fun Int.primeFactors(): List<Int> {
+            var remainingValue = this
+            var testFactor = 2
+            val primes = mutableListOf<Int>()
+
+            while (testFactor * testFactor <= remainingValue)
+                if (remainingValue % testFactor == 0) {
+                    primes.add(testFactor)
+                    remainingValue /= testFactor
+                } else
+                    testFactor += 1
+
+            if (remainingValue > 1)
+                primes.add(remainingValue)
+
+            return primes
+        }
+
+        Assert.assertEquals(listOf(3, 3, 3, 3), 81.primeFactors())
     }
 }
