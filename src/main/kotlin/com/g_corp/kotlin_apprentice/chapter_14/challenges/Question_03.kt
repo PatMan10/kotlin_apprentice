7074d7a2db45547ce79228337150646d5a349647
PatMan10 <PatMan10@protonmail.com>
2020-07-18 14:41:54 +0200

    chapter 14 challenge 3 done

--- src/main/kotlin/com/g_corp/kotlin_apprentice/chapter_14/challenges/Question_03.kt
+++ src/main/kotlin/com/g_corp/kotlin_apprentice/chapter_14/challenges/Question_03.kt
@@ -3,3 +3,9 @@ package com.g_corp.kotlin_apprentice.chapter_14.challenges
 fun main(args: Array<String>) {
     println("Challenge Question")
 }
+
+object MyMath {
+    fun isEven(n: Int) = n % 2 == 0
+
+    fun isOdd(n: Int) = n % 2 != 0
+}
--- src/test/kotlin/com/g_corp/kotlin_apprentice/chapter_14/challenges/Question_03Test.kt
+++ src/test/kotlin/com/g_corp/kotlin_apprentice/chapter_14/challenges/Question_03Test.kt
@@ -5,7 +5,21 @@ import org.junit.Test
 
 public class Question_03Test {
     @Test
-    fun testSomething() {
-        Assert.assertEquals(1, 1)
+    fun testMyMathClass() {
+        Assert.assertFalse(MyMath.isEven(1))
+        Assert.assertFalse(MyMath.isEven(3))
+        Assert.assertFalse(MyMath.isEven(5))
+
+        Assert.assertTrue(MyMath.isEven(2))
+        Assert.assertTrue(MyMath.isEven(4))
+        Assert.assertTrue(MyMath.isEven(6))
+
+        Assert.assertTrue(MyMath.isOdd(1))
+        Assert.assertTrue(MyMath.isOdd(3))
+        Assert.assertTrue(MyMath.isOdd(5))
+
+        Assert.assertFalse(MyMath.isOdd(2))
+        Assert.assertFalse(MyMath.isOdd(4))
+        Assert.assertFalse(MyMath.isOdd(6))
     }
 }
