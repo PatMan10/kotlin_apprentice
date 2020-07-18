bd5eca8f026aa97e47e461817be117cf411e5b7e
PatMan10 <PatMan10@protonmail.com>
2020-07-18 14:48:00 +0200

    chapter 14 challenge 4 done

--- src/test/kotlin/com/g_corp/kotlin_apprentice/chapter_14/challenges/Question_04Test.kt
+++ src/test/kotlin/com/g_corp/kotlin_apprentice/chapter_14/challenges/Question_04Test.kt
@@ -5,7 +5,24 @@ import org.junit.Test
 
 public class Question_04Test {
     @Test
-    fun testSomething() {
-        Assert.assertEquals(1, 1)
+    fun testExtensionMethods() {
+        fun Int.isEven() = this % 2 == 0
+        fun Int.isOdd() = this % 2 != 0
+
+        Assert.assertTrue(1.isOdd())
+        Assert.assertTrue(3.isOdd())
+        Assert.assertTrue(5.isOdd())
+
+        Assert.assertFalse(1.isEven())
+        Assert.assertFalse(3.isEven())
+        Assert.assertFalse(5.isEven())
+
+        Assert.assertTrue(2.isEven())
+        Assert.assertTrue(4.isEven())
+        Assert.assertTrue(6.isEven())
+
+        Assert.assertFalse(2.isOdd())
+        Assert.assertFalse(4.isOdd())
+        Assert.assertFalse(6.isOdd())
     }
 }
